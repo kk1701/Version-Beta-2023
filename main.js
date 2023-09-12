da54ebeee8a0963257fde4d380ee2920e4d63791
kk1701 <karankute05@gmail.com>
2023-09-12 19:51:32 +0530

    reponse

--- main.js
+++ main.js
@@ -26,3 +26,8 @@ function scrollToTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
 
+
+
+
+
+
--- timerclock.js
+++ timerclock.js
@@ -1,4 +1,5 @@
-const countdownDate = new Date("September 22, 2023 00:00:00").getTime();
+const countdownDate = new Date("September 22, 2023 00:30:00").getTime();
+
 
 const countdownInterval = setInterval(function () {
     const currentDate = new Date().getTime();
@@ -10,10 +11,76 @@ const countdownInterval = setInterval(function () {
     const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
     const countdownElement = document.getElementById("clock");
-    countdownElement.innerHTML = `${days} : ${hours} : ${minutes} : ${seconds}`;
+
+    if(days<10){
+        if(hours<10){
+            if(minutes<10){
+                if(seconds<10){
+                    countdownElement.innerHTML = `0${days} : 0${hours} : 0${minutes} : 0${seconds}`;
+                }else{
+                    countdownElement.innerHTML = `0${days} : 0${hours} : 0${minutes} : ${seconds}`;
+                }
+            }else{
+                if(seconds<10){
+                    countdownElement.innerHTML = `0${days} : 0${hours} : ${minutes} : 0${seconds}`;
+                }else{
+                    countdownElement.innerHTML = `0${days} : 0${hours} : ${minutes} : ${seconds}`;
+                }
+            }
+        }else{
+            if(minutes<10){
+                if(seconds<10){
+                    countdownElement.innerHTML = `0${days} : ${hours} : 0${minutes} : 0${seconds}`;
+                }else{
+                    countdownElement.innerHTML = `0${days} : ${hours} : 0${minutes} : ${seconds}`;
+                }
+            }else{
+                if(seconds<10){
+                    countdownElement.innerHTML = `0${days} : ${hours} : ${minutes} : 0${seconds}`;
+                }else{
+                    countdownElement.innerHTML = `0${days} : ${hours} : ${minutes} : ${seconds}`;
+                }
+            }
+        }
+    }else{
+        if(hours<10){
+            if(minutes<10){
+                if(seconds<10){
+                    countdownElement.innerHTML = `${days} : 0${hours} : 0${minutes} : 0${seconds}`;
+                }else{
+                    countdownElement.innerHTML = `${days} : 0${hours} : 0${minutes} : ${seconds}`;
+                }
+            }else{
+                if(seconds<10){
+                    countdownElement.innerHTML = `${days} : 0${hours} : ${minutes} : 0${seconds}`;
+                }else{
+                    countdownElement.innerHTML = `${days} : 0${hours} : ${minutes} : ${seconds}`;
+                }
+            }
+        }else{
+            if(minutes<10){
+                if(seconds<10){
+                    countdownElement.innerHTML = `${days} : ${hours} : 0${minutes} : 0${seconds}`;
+                }else{
+                    countdownElement.innerHTML = `${days} : ${hours} : 0${minutes} : ${seconds}`;
+                }
+            }else{
+                if(seconds<10){
+                    countdownElement.innerHTML = `${days} : ${hours} : ${minutes} : 0${seconds}`;
+                }else{
+                    countdownElement.innerHTML = `${days} : ${hours} : ${minutes} : ${seconds}`;
+                }
+            }
+        }
+    }
+    
 
     if (timeLeft < 0) {
         clearInterval(countdownInterval);
-        countdownElement.innerHTML = "00:00:00";
+        countdownElement.innerHTML = "00 : 00 : 00 : 00";
     }
 }, 1000);
+
+
+const colon = document.getElementsByClassName("colon");
+colon.style.color = "red";
